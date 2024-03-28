20093d1dde0e4bb120c9a672b44ac6146c0c8303
WalterCorsini <corsini.walter@gmail.com>
2024-03-28 17:58:13 +0100

    add style dadi

--- js-dadi/js/script.js
+++ js-dadi/js/script.js
@@ -8,11 +8,13 @@ let result = 0
 
 // eventlistener Click button
 sendBtnElem.addEventListener("click", function(){
+    // reset variable
+    document.getElementById("dadi-pc").innerHTML = "";
+
     // save input mail to variable
     dadiPc = Math.floor(Math.random() * 6 + 1);
     dadiUser = Math.floor(Math.random() * 6 + 1);
-    console.log(dadiPc, "dadi pc");
-    console.log(dadiUser, "dadi user");
+
 
     if(dadiPc > dadiUser){
         result = 1;
@@ -21,9 +23,26 @@ sendBtnElem.addEventListener("click", function(){
     } else {
         result = 0;
     }
-    document.getElementById("dadi-pc").innerHTML = "DADO DEL COMPUTER  " + dadiPc;
-    document.getElementById("dadi-user").innerHTML = "IL TUO DADO  " + dadiUser;
+
+
 // OUTPUT
+    // result PC
+    for(let i = 1;  i <= 6; i++){
+        if(dadiPc == i){
+            document.getElementById("dadi-pc").innerHTML = "<h2>Dado del computer</h2><div class=dado"+dadiPc+"></div>";
+
+        }
+}
+
+    // result USER
+    for(let i = 1;  i <= 6; i++){
+        if(dadiPc == i){
+            document.getElementById("dadi-user").innerHTML = "<h2>Dado utente</h2><div class=dado"+dadiUser+"></div>";
+
+        }
+}
+
+
     if(result === 1){
         document.getElementById("result").innerHTML = "HAI PERSO"
     } else if(result === 2){
